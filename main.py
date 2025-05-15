9c20abdf36d40484bad5fc3220597ab101c35d08
Vlad19540704 <eglvlad2025@outlook.com>
2025-05-15 13:10:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
- from flask import Flask, request
+from flask import Flask, request
 import requests
 import os
 from openai import OpenAI
@@ -41,4 +40,5 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
